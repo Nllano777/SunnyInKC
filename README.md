7aef51dad0d39d3b7f8e234ef5c1cf4f61a6906a
Nllano777 <106032601+Nllano777@users.noreply.github.com>
2022-09-19 00:01:31 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,77 @@
+
+
+# The Weather App
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+---
+
+## Table of Contents
+
+* [Title](#Title)
+* [Link](#Link)
+* [Description](#Description)
+* [Installation](#Installation)
+* [Licenses](#Licenses)
+* [Contributing](#Contributing)
+* [Contact](#Contact)
+* [Credits](#Credits)
+    
+---
+
+## Description
+
+Users can check and see the weather outlook for multiple cities in real time.
+
+---
+
+## Link 
+
+https://nllano777.github.io/TheWeather/
+
+---
+
+## Repository
+
+https://github.com/Nllano777/TheWeather
+
+---
+
+## Installation
+
+Use in Browser.
+
+---
+
+## License  
+    This project is covered under the MIT license.
+
+---
+
+## Contributing
+
+Contribute as you wish.
+
+To contribute to this application, create a pull request.
+Here are the steps needed for doing that:
+- Fork the repo
+- Create a feature branch (git checkout -b NAME-HERE)
+- Commit your new feature (git commit -m 'Add some feature')
+- Push your branch (git push)
+- Create a new Pull Request
+Following a code review, your feature will be merged.
+
+---
+
+## Contact
+
+GitHub: https://github.com/Nllano777  
+Email: nllbr99@gmail.com
+
+---
+
+## Credits
+
+Nicolas Llano
+
+  
---
+++ logic.js
@@ -0,0 +1,65 @@
+// Create Variables 
+var input = document.getElementById('input_text');
+var city = document.getElementById('City');
+var temperature = document.getElementById('Temperature');
+var wind = document.getElementById('Wind');
+var humidity = document.getElementById('Humidity');
+var uvIndex = document.getElementById('uvIndex');
+var button = document.getElementById('submit');
+var APIKey = "a7cef51d92248879613b36f93ac710ed";
+
+// Create a fetch
+function getWeatherInfo() {
+    var rawCity = document.getElementById("rawcity").value;
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + rawCity + "&appid=a7cef51d92248879613b36f93ac710ed";
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+
+
+            let lat = data.coord.lat;
+            let lon = data.coord.lon;
+
+            fetch("http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,hourly,minutely,alerts&appid=" + APIKey)
+                .then(response => response.json())
+                .then(function (response) {
+                    console.log(response)
+                    // For each loop
+                    var Ind = 1; // create variable to increase
+                    response.daily.forEach(element => {
+                        if (Ind < 6) { // 6 elements
+                            var newdate = new Date(element.dt * 1000).toLocaleDateString();
+
+                            city = document.getElementById("City" + Ind);
+                            city.innerHTML = data.name + "(" + newdate + ")"
+
+                            temperature = document.getElementById("Temperature" + Ind);
+                            temperature.innerHTML = "Temperature: " + element.temp.day
+
+                            wind = document.getElementById("Wind" + Ind);
+                            wind.innerHTML = "Wind: " + element.wind_speed
+
+                            humidity = document.getElementById("Humidity" + Ind);
+                            humidity.innerHTML = "Humidity: " + element.humidity
+
+                            uvIndex = document.getElementById("uvIndex" + Ind)
+                            uvIndex.innerHTML = "uvIndex: " + element.uvi
+                            Ind++;
+
+                        }
+                    });
+                })
+        })
+};
+// key-up Enter to get weather info.
+document.getElementById("rawcity").addEventListener('keyup', function (event) {
+    if (event.keyCode === 13) {
+        getWeatherInfo();
+    }
+});
+// press Button to get weather info.
+button.addEventListener('click', function () {
+    getWeatherInfo();
+});
+
